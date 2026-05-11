828ffb745911920a8c61bc71db30c2c0f58c6d40
Aditya Dwi Irawan <900275@hq.bni.co.id>
2026-05-11 16:18:53 +0700

    first commit

---
+++ Tailwind-Project/Tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2563eb",
+        secondary: "#9333ea",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
