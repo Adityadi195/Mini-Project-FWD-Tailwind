2173156dc456e2330a89cf0458c8a37971c279e1
Aditya Dwi Irawan <900275@hq.bni.co.id>
2026-05-15 20:55:51 +0700

    [ASSIGNMENT] DAY - 18 Mini Project Full-stack Web Development Tailwind final

--- Tailwind-Project/Tailwind.config.js
+++ Tailwind-Project/Tailwind.config.js
@@ -1,17 +1,47 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
-    "./src/**/*.{html,js}"
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      colors: {
-        primary: "#2563eb",
-        secondary: "#9333ea",
-      },
       fontFamily: {
-        sans: ["Inter", "sans-serif"],
+        lexend: ['Lexend', 'sans-serif'],
+      },
+      colors: {
+        primary: '#125CED',
+        dark: {
+          900: '#0D121C',
+        },
+        text: {
+          primary: '#0D121C',
+          secondary: '#4D6699',
+        },
+        slate: {
+          50: '#F7FAFC',
+        },
+        gray: {
+          100: '#E8EBF2',
+          200: '#E6E8EB',
+          300: '#CFD6E8',
+        },
+      },
+      spacing: {
+        '65px': '65px',
+      },
+      width: {
+        '301px': '301px',
+        '597px': '597px',
+        '896px': '896px',
+      },
+      height: {
+        '65px': '65px',
+        '176px': '176px',
+        '169px': '169px',
+        '178px': '178px',
       },
     },
   },
   plugins: [],
-}
+};
